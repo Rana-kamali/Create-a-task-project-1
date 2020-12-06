1af1ae892c897238aff6ec12074816a181eed98f
Rana Kamali <raana.kamalinezhad@gmail.com>
2020-12-06 17:38:53 +1100

    Name Input value added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 console.log("app is running");
+/// P1 New clicked
+$( ".grid-item-right" ).click(function() {
+    alert( "You are creating a new task" );
+    console.log("Creating a new task");
+  });
+
+  // P2 Add project name
+
+
+$(".btn-primary").on("click", () => {
+  console.log("was clicked!");
+  const input = $(".nameInput").val();
+  console.log(input);
+$(".nameInput").append(input);
+});
