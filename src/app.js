01ffa9f3c1a1fe40cb21950abcfbcd7429db38ed
Rana Kamali <raana.kamalinezhad@gmail.com>
2020-12-12 19:54:49 +1100

    delete button is working

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 console.log("app is running");
+let id = 0;
 ///list
 // const todoList = [{
 // //   name : "",
 // //   status: "",
 // //   date: "",
 // //   comment: "",
-
-
 // } ];
-const todoList=[];
+const todoList = [];
 // console.log("printing to do list", todolist);
 $(".resultpage").append('<ul id="todos"></ul>');
 todoList.forEach((element, i) => {
@@ -19,24 +18,17 @@ todoList.forEach((element, i) => {
 //   todoList.push({});
 // }
 console.log(todoList);
-
 /// P1 New clicked
 $(".grid-item-right").click(function () {
   // alert( "You are creating a new task" );
   console.log("Creating a new task");
 });
-
 const $container = $(`.modal-header`);
 // console.log(" container is : ", $container);
-
 const $h1 = `<h1>create a task</h1>`;
 // console.log(`h1 is working`, $h1);
 $container.append($h1);
-
-
-
 const $modalBody = $(`.modal-body`);
-
 const $form = `
   <form id="toDoForm">
     <label>Name</label>
@@ -55,9 +47,9 @@ const $form = `
     <button type="submit" class="btn btn-primary btn-sm">Submit</button>
   </form>
 `;
-const renderNewToDoItem = (newToDo) =>{
+const renderNewToDoItem = (newToDo) => {
   return ` 
-  <tr class="heading">
+  <tr class="heading" id="heading-${newToDo.id}">
   <th>Action</th>
   <th>Edit</th>
   <th >Project Name</th>
@@ -65,25 +57,22 @@ const renderNewToDoItem = (newToDo) =>{
   <th>Project Date</th>
   <th>Comment</th>
 <li>
-  
 </tr>
-<tr>
-  <td><i class="fa fa-trash"></i></td>
+<tr id="row-${newToDo.id}">
+  <td><i id=${newToDo.id} class="fa fa-trash"></i></td>
   <td><span class="glyphicon">&#x270f;</span></td>
   <td class="nameoftheproject">${newToDo.name}</td>
   <td class="nameofstatus">${newToDo.status}</td>
   <td class="resultofdate">${newToDo.date}</td>
   <td class="resultofcomment">${newToDo.comment}</td>
-  
 </tr>
 <tr>
-  `
-}
-
+  `;
+};
 $modalBody.append($form);
 $(function () {
   $("#datepicker").datepicker();
-  format: 'dd/mm/yyyy'
+  format: "dd/mm/yyyy";
 });
 $("#toDoForm").on("submit", (event) => {
   event.preventDefault();
@@ -91,54 +80,41 @@ $("#toDoForm").on("submit", (event) => {
   const status = $("select[name='status']").val();
   const date = $("input[name='date']").val();
   const comment = $("textarea[name='comment']").val();
+  id = id + 1;
   const newTodoItem = {
+    id: id,
     name: name,
     status: status,
     date: date,
     comment: comment,
   };
-  console.log("new item: ", newTodoItem);
-  todoList.push(newTodoItem);
-  const newToDoItemIndex= todoList.length-1;
-  console.log("newtodo index",newToDoItemIndex);
-  console.log("after push", todoList);
   const $newToDo = $(renderNewToDoItem(newTodoItem));
-  $newToDo.find(".fa-trash").on("click",()=>{
-    todoList.splice(newToDoItemIndex,1);
-    console.log("removing an item", todoList);
-    console.log("new to do clicked");
-  })
   console.log($newToDo);
   $("#resultpage").append($newToDo);
-
 });
-
-// const test = (obj) => { 
-//   console.log(obj);
-//   console.log("Delete button should work!")
-//   todoList.pop(obj);
-// }
-
-
 //// Make my Day//
-
-
-const quotes = $.get(`https://quote-garden.herokuapp.com/api/v2/quotes/random`,(data) =>{
-  console.log(data);
-  const quoteAuthor = data.quote.quoteAuthor;
-  const quote = data.quote.quoteText;
-  console.log(quote);
-  console.log(quoteAuthor);
-  const text = `<p>${quote}</p>`;
-  const author = `<p>${quoteAuthor}</p>`;
-  console.log(author)
-  console.log(text);
-
-  $("#makeMyday").on("click", () => {
-    $("#Qoute").append(text);
-    $("#Author").append(author);
-  });
+$("#resultpage").on("click", "i.fa-trash", (e) => {
+  console.log("this was pressed");
+  const idToBeDeleted = e.target.id;
+  console.log(idToBeDeleted);
+  $(`#heading-${idToBeDeleted}`).remove();
+  $(`#row-${idToBeDeleted}`).remove();
 });
-
-
-
+const quotes = $.get(
+  `https://quote-garden.herokuapp.com/api/v2/quotes/random`,
+  (data) => {
+    console.log(data);
+    const quoteAuthor = data.quote.quoteAuthor;
+    const quote = data.quote.quoteText;
+    console.log(quote);
+    console.log(quoteAuthor);
+    const text = `<p>${quote}</p>`;
+    const author = `<p>${quoteAuthor}</p>`;
+    console.log(author);
+    console.log(text);
+    $("#makeMyday").on("click", () => {
+      $("#Qoute").append(text);
+      $("#Author").append(author);
+    });
+  }
+);
